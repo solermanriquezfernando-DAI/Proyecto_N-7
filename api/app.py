54f3b2febb00cde0c5af121433f129caa8e7c14e
solermanriquezfernando-DAI <solermanriquezfernando@gmail.com>
2025-11-01 22:52:43 -0300

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
 import joblib
 import pandas as pd
+import os
 
 app = FastAPI()
 
+# Ruta del modelo (funciona local y en GitHub + Colab)
+model_path = os.path.join(os.path.dirname(__file__), "..", "models", "best_rf_model.pkl")
+
 # Cargar modelo
-modelo = joblib.load("../models/best_rf_model.pkl")
+modelo = joblib.load(model_path)
 
 @app.post("/predict")
 def predict(data: dict):
